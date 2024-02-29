5275bf05b77653c3b97b326b9e8b748bf1af579b
unknown <matijailicmatke@gmail.com>
2024-02-29 17:22:20 +0100

    Had a problem with the master branch here is what was in privious deleted commits: creation of project, login, register, front end making, auth for users, and now the last one is read jobs for user and also done some front end issues.

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -214,7 +214,7 @@ class AccountController extends Controller
             $job->title = $request->title;
             $job->category_id = $request->category;
             $job->job_type_id  = $request->jobType;
-            // $job->user_id = Auth::user()->id;
+            $job->user_id = Auth::user()->id;
             $job->vacancy = $request->vacancy;
             $job->salary = $request->salary;
             $job->location = $request->location;
@@ -245,8 +245,11 @@ class AccountController extends Controller
     }
 
      public function myJobs() {    
+        $jobs = Job::where('user_id',Auth::user()->id)->with('jobType')->paginate(10);   
              
-        return view('front.account.job.my-jobs');
+        return view('front.account.job.my-jobs',[
+            'jobs' => $jobs
+        ]);
     }  
 
 }
--- app/Models/Job.php
+++ app/Models/Job.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Job extends Model
 {
     use HasFactory;
+
+    public function jobType() {
+        return $this->belongsTo(JobType::class);
+    }
+
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+
+    // public function applications() {
+    //     return $this->hasMany(JobApplication::class);
+    // }
+
+    // public function user() {
+    //     return $this->belongsTo(User::class);
+    // }
 }
