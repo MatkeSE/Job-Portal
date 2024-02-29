50700b87055e249528b760a5f465090b5a88fbcb
unknown <matijailicmatke@gmail.com>
2024-02-29 16:41:41 +0100

    Had problem with user_id, so returned to previus commit, and then i did add jobs funciton successfully

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    //Home page
+    public function index()
+    {
+      return view('front.home');
+    }
+
+   
+}
